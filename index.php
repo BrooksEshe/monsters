3b4e30d4fea50f261883594f40d089237531c513
Brooks Eshe <beshe@mail.greenriver.edu>
2019-02-06 14:50:47 -0500

    finished

--- index.php
+++ index.php
@@ -7,8 +7,27 @@ require_once('vendor/autoload.php');
 //create an instance of the Base class
 $f3 = Base::instance();
 
+//include classes
+require ('classes/Monster.php');
+require ('classes/Vampire.php');
+require ('classes/Warewolf.php');
+
 //define a default route
 $f3->route('GET /', function(){
+    $monster = new Monster("Fred");
+    $vampire = new Vampire("Doug");
+    $werewolf = new Warewolf("Chuck");
+
+    //$firstAttack = $vampire->attack();
+    //$f3->set('vampire',$firstAttack);
+
+    echo $monster->attack(). "<br>";
+    echo $vampire ->attack(). "<br>";
+    echo $vampire->attack(). "<br>";
+    echo $werewolf->attack(). "<br>";
+
     $template = new Template();
     echo $template->render('views/home.html');
 });
+
+$f3->run();
