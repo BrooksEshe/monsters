bc1131196df749145a1dacf76ba332f24111f579
Brooks Eshe <beshe@mail.greenriver.edu>
2019-02-06 14:15:46 -0500

    added basic files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+//create an instance of the Base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function(){
+    $template = new Template();
+    echo $template->render('views/home.html');
+});
